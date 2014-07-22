a92c89b61c987ba448c79fd4798085a219b39dbd
tianlitao <2423806988@qq.com>
2014-07-22 17:13:22 +0800

    存储bids数组

--- data_structure_1/spec/bid_spec.js
+++ data_structure_1/spec/bid_spec.js
@@ -1,7 +1,5 @@
 
 describe("Bid", function() {
-
-
     beforeEach(function() {
         init_activity_database();
         var activities = JSON.parse(localStorage.activities);
@@ -10,21 +8,19 @@ describe("Bid", function() {
             sign_ups: [],
             bids: []
         };
-
         activities.push(activity);
-
         localStorage.activities = JSON.stringify(activities);
 //        console.log(localStorage.activities)
         localStorage.current_activity = "first activity";
         localStorage.is_bidding = "";
     });
-
     afterEach(function(){
         localStorage.clear();
     })
 
     it("should create new bid", function(){
         create_new_bid("first activity");
+        console.log(localStorage.activities)
 
         var activities = JSON.parse(localStorage.activities);
         expect(activities[0].bids.length).toBe(1);
--- data_structure_1/src/bid.js
+++ data_structure_1/src/bid.js
@@ -2,3 +2,12 @@ function Bid(){
     this.name="竞价1"
     this.biddings=[]
 }
+create_new_bid=function(){
+    var activities = JSON.parse(localStorage.getItem("activities"))
+   var act= _.find(activities, function (act) {
+        return act.name == "first activity"
+    })
+    var bid=new Bid()
+    act.bids.unshift(bid)
+    localStorage.setItem("activities", JSON.stringify(activities))
+}
