e1cbfd2a5969699259cb066ded0c502ddeb4e52b
tianlitao <2423806988@qq.com>
2014-07-22 19:04:02 +0800

    检查是否电话重复

--- data_structure_1/spec/sign_up_spec.js
+++ data_structure_1/spec/sign_up_spec.js
@@ -20,7 +20,6 @@ describe("SignUp", function() {
         var activities = JSON.parse(localStorage.activities);
         expect(activities[1].sign_ups.length).toBe(1);
         expect(activities[1].sign_ups[0].name).toBe("仝键");
-
     });
 
     it("should one sms with signing up content sign up failed when user has signed", function(){
@@ -30,6 +29,7 @@ describe("SignUp", function() {
         notify_sms_received(sms_json);
 
         var activities = JSON.parse(localStorage.activities);
+        console.log(activities)
         expect(activities[1].sign_ups.length).toBe(1);
         expect(activities[1].sign_ups[0].name).toBe("仝键");
     });
--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -1,3 +1,16 @@
-function notify_sms_received(){
-
+function notify_sms_received(sms_json){
+    var message = sms_json.messages[0].message.replace(/\s/g, "");
+    var activities = JSON.parse(localStorage.getItem("activities"))
+    var apply_name = message.substr(2).trim()
+    var apply_phone = sms_json.messages[0].phone
+    var apply_array = {'name': apply_name, 'phone': apply_phone}
+    var act = _.findWhere(activities, {name: localStorage.current_activity}).sign_ups
+var judge= _.find(act, function (act){
+        return act.phone == apply_phone
+    })
+    if(!judge) {
+        console.log("2")
+        _.findWhere(activities, {name: localStorage.current_activity}).sign_ups.push(apply_array)
+        localStorage.setItem("activities", JSON.stringify(activities))
+    }
 }
