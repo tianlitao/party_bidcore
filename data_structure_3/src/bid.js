b7a553f20c5ced5c162ccf028b8935c1c56ca861
tianlitao <2423806988@qq.com>
2014-07-24 10:34:14 +0800

    第三种数据结构报名信息的判断

---
+++ data_structure_3/src/bid.js
@@ -0,0 +1,8 @@
+function Bid() {
+    this.name = "竞价1"
+    this.biddings = []
+}
+
+create_new_bid=function(){
+
+}
--- data_structure_3/src/notify_sms_received.js
+++ data_structure_3/src/notify_sms_received.js
@@ -1,10 +1,12 @@
 function notify_sms_received(sms_json) {
-    if (localStorage.is_signing_up = "true") {
+    if (localStorage.is_signing_up == "true" && !check_activity_phone_repeat(sms_json)) {
         save_sign_up_message(sms_json)
     }
 }
-check_activity_phone_repeat=function(){
-    
+check_activity_phone_repeat=function(sms_json){
+    var sign_ups = JSON.parse(localStorage.getItem("sign_ups"))
+    var apply_phone = sms_json.messages[0].phone
+   return( _.find(sign_ups,function(sign_ups){return sign_ups.phone==apply_phone}))
 }
 save_sign_up_message = function (sms_json) {
     var sign_ups = JSON.parse(localStorage.getItem("sign_ups"))
